f70c11fb958b5a5ecbd562838656b46374b4703c
Trevor Old <trevor.old@outlook.com>
2018-01-31 10:28:25 -0800

    Implemented rendering of two grocery items

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+var GroceryList = () => (
+    <ul>
+       <Milk />
+        <Eggs />
+    </ul>
+);
+
+var Milk = () => (
+    <li>Milk</li>
+);
+
+var Eggs = () => (
+    <li>Eggs</li>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));

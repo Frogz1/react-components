eb264d2632c4f632cc57e8fb5afce2c6cafcab2e
Trevor Old <trevor.old@outlook.com>
2018-01-31 11:06:10 -0800

    Dynamically generating grocery list and applying bold on hover and removing on mouseleave

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 // TODO
 
-var GroceryListItem = (props) => (
+// var GroceryListItem = (props) => (
+//     <ul>
+//         <li>{props.items[0]}</li>
+//         <li>{props.items[1]}</li>
+//     </ul>
+// );
+class GroceryListItem extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            done: false
+        };
+    }
+    onListItemHover() {
+        this.setState({
+            done: !this.state.done,
+            isHovered: !this.state.isHovered
+        });
+    }
+
+    render() {
+        var style = {
+            fontWeight: this.state.isHovered ? 'bold' : ''
+        };
+        return (
+            <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)} >{this.props.item}</li>
+        )
+    }
+}
+
+var GroceryList = (props) => (
     <ul>
-        <li>{props.items[0]}</li>
-        <li>{props.items[1]}</li>
-    </ul>
-);
-var GroceryList = () => (
-    <ul>
-        <GroceryListItem items={['Milk', 'Eggs']}/>
+        {props.items.map(item =>
+        <GroceryListItem item={item} />
+        )}
     </ul>
 );
 
-var Milk = () => (
-    <li>Milk</li>
+var App = () => (
+    <ul>
+        <GroceryList items={['Milk', 'Eggs', 'Bread', 'Cereal']}/>
+    </ul>
 );
-
-var Eggs = () => (
-    <li>Eggs</li>
-);
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
